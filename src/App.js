dec6f3908236a8eac6c66de7066939b18a819dac
Colin Champdavoine <colinchampdavoine@Colins-MacBook-Pro.local>
2020-11-23 17:07:44 +0100

    random

--- src/App.js
+++ src/App.js
@@ -3,22 +3,31 @@ import React, { useState } from "react";
 import TinderCard from "react-tinder-card";
 import "./App.css";
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 const generateList = (length) => {
-  for (let i = 0; i < length; i++) {}
+  let list = [];
+  for (let i = 0; i < length; i++) {
+    list.push(`https://cdn.porngifs.com/img/${getRandomInt(5000)}`);
+  }
+  console.log("🚀 ~ file: App.js ~ line 12 ~ generateList ~ list", list);
+  return list;
 };
 
 function Simple() {
-  const characters = db;
+  const [list, setList] = useState(generateList(5));
   const [lastDirection, setLastDirection] = useState();
 
-  const swiped = (direction, nameToDelete) => {
-    console.log("removing: " + nameToDelete);
-    setLastDirection(direction);
+  const swiped = (direction, nameToDelete, index) => {
+    console.log("🚀 ~ file: App.js ~ line 36 ~ swiped ~ index", index);
+    if (index === 0) {
+      setList(generateList(10));
+    }
   };
 
-  const outOfFrame = (name) => {
-    console.log(name + " left the screen!");
-  };
+  const outOfFrame = (url) => {};
 
   return (
     <div>
@@ -32,19 +41,24 @@ function Simple() {
       />
       {/* <h1>La GIFle</h1> */}
       <div className="cardContainer">
-        {characters.map((character) => (
-          <TinderCard
-            className="swipe"
-            key={character.name}
-            onSwipe={(dir) => swiped(dir, character.name)}
-            onCardLeftScreen={() => outOfFrame(character.name)}
-          >
-            <div
-              style={{ backgroundImage: "url(" + character.url + ")" }}
-              className="card"
-            ></div>
-          </TinderCard>
-        ))}
+        {list ? (
+          list.map((url, index) => (
+            <TinderCard
+              className="swipe"
+              key={index + url}
+              preventSwipe={["up", "down"]}
+              onSwipe={(dir) => swiped(dir, url, index)}
+              onCardLeftScreen={() => outOfFrame(url)}
+            >
+              <div
+                style={{ backgroundImage: "url(" + url + ")" }}
+                className="card"
+              ></div>
+            </TinderCard>
+          ))
+        ) : (
+          <h1>hello</h1>
+        )}
       </div>
     </div>
   );
