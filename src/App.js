79157c9d719cd81a06d493cc871cd0f922d017da
Colin Champdavoine <colinchampdavoine@Colins-MacBook-Pro.local>
2020-11-23 17:07:44 +0100

    random

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import React, { useState } from "react";
 import TinderCard from "react-tinder-card";
 import "./App.css";
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 const generateList = (length) => {
-  for (let i = 0; i < length; i++) {}
+  let list = [];
+  for (let i = 0; i < length; i++) {
+    list.push(`https://cdn.porngifs.com/img/${getRandomInt(5000)}`);
+  }
+  return list;
 };
 
 function Simple() {
-  const characters = db;
+  const characters = generateList(5);
   const [lastDirection, setLastDirection] = useState();
 
   const swiped = (direction, nameToDelete) => {
@@ -32,15 +40,15 @@ function Simple() {
       />
       {/* <h1>La GIFle</h1> */}
       <div className="cardContainer">
-        {characters.map((character) => (
+        {characters.map((url) => (
           <TinderCard
             className="swipe"
-            key={character.name}
-            onSwipe={(dir) => swiped(dir, character.name)}
-            onCardLeftScreen={() => outOfFrame(character.name)}
+            key={url}
+            onSwipe={(dir) => swiped(dir, url)}
+            onCardLeftScreen={() => outOfFrame()}
           >
             <div
-              style={{ backgroundImage: "url(" + character.url + ")" }}
+              style={{ backgroundImage: "url(" + url + ")" }}
               className="card"
             ></div>
           </TinderCard>
